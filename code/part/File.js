c65b944f90fc797ce7d9f1f94e0182c2ec0fcbcd
DanoR <zheung@vip.qq.com>
2019-12-20 17:31:39 +0800

    log tweak

--- code/part/File.js
+++ code/part/File.js
@@ -15,18 +15,27 @@ module.exports = function File(name, fileSize, link, langs, fileChunks, version)
 	this.version = version;
 
 	this.extract = async function(version, cdn) {
-		L(`[File] ${this.name}`);
-
 		let bundleIDSet = new Set();
 
 		this.fileChunks.forEach(chunk => bundleIDSet.add(chunk.bundleID));
 
-		let bundleBuffer = {};
-		for(let bundleID of bundleIDSet) {
-			let [bid, buffer] = await fetchBundle(bundleID, version, cdn);
+		L(`[File] ${this.name} length ${bundleIDSet.size}`);
 
-			bundleBuffer[bid] = buffer;
+		let bundleBuffer = {};
+
+		const promises = [];
+		for(let bundleID of bundleIDSet) {
+			promises.push(fetchBundle(bundleID, version, cdn).then(([bid, buffer]) => bundleBuffer[bid] = buffer));
 		}
+		await Promise.all(promises);
+
+		// for(let bundleID of bundleIDSet) {
+		// 	let [bid, buffer] = await fetchBundle(bundleID, version, cdn);
+
+		// 	bundleBuffer[bid] = buffer;
+		// }
+
+		L(`[File] ${this.name} AllFetched, UnZstding...`);
 
 		let pathFinal = _pa.join('./assets/', String(this.version), this.name);
 
